da7fcbd7cffc6250d09b2c91a4c5bac8a39ca87d
Nayeon Shin <dev.nayeon@gmail.com>
2023-12-01 11:37:13 -0500

    docs: add test results to

--- src/extended_concurrency.py
+++ src/extended_concurrency.py
@@ -11,19 +11,19 @@ def calculate_sum_in_range(start: int, end: int) -> int:
 
 
 def calculate_for_process(start: int, end: int, name: str) -> None:
-    # start_time = time()
+    start_time = time()
     result = calculate_sum_in_range(start, end)
-    # end_time = time()
+    end_time = time()
     print(f"MULTIPROCESSING - {name} sum: {result}")
-    # print(TIME_MESSAGE.format(end_time - start_time))
+    print(TIME_MESSAGE.format(end_time - start_time))
 
 
 def calculate_for_thread(start: int, end: int) -> None:
-    # start_time = time()
+    start_time = time()
     result = calculate_sum_in_range(start, end)
-    # end_time = time()
+    end_time = time()
     print(f"MULTITHREADING - sum: {result}")
-    # print(TIME_MESSAGE.format(end_time - start_time))
+    print(TIME_MESSAGE.format(end_time - start_time))
 
 
 def sum_using_multiprocessing(bounds: tuple[str]) -> None:
@@ -45,7 +45,7 @@ def sum_using_multithreading(bounds: tuple[int]) -> None:
     thread1 = Thread(target=calculate_for_thread, args=(start1, end1))
     thread2 = Thread(target=calculate_for_thread, args=(start2, end2))
 
-    # start_time = time()
+    start_time = time()
 
     thread1.start()
     thread2.start()
@@ -53,15 +53,15 @@ def sum_using_multithreading(bounds: tuple[int]) -> None:
     thread1.join()
     thread2.join()
 
-    # end_time = time()
-    # message = "Total " + TIME_MESSAGE[0].lower() + TIME_MESSAGE[1:]
-    # print(message.format(end_time - start_time))
+    end_time = time()
+    message = "Total " + TIME_MESSAGE[0].lower() + TIME_MESSAGE[1:]
+    print(message.format(end_time - start_time))
 
 
 if __name__ == "__main__":
     bounds = 1, MAX_NUMBER // 2, (MAX_NUMBER // 2) + 1, MAX_NUMBER
 
-    # for _ in range(10):
-    sum_using_multiprocessing(bounds)
-    # for _ in range(10):
-    sum_using_multithreading(bounds)
+    for _ in range(10):
+    	sum_using_multiprocessing(bounds)
+    for _ in range(10):
+    	sum_using_multithreading(bounds)
